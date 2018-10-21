556a993d62968e850b991037fbba2222dc83b3b6
Robin Tram <robin.tram@gmail.com>
2018-10-21 15:52:21 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,7 +32,8 @@ def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
   
   Movie.create do |m|
     binding.pry
-    title: ''
+    title: args[:title]
+    release_date: args[:release_date]
   end
 end
 
