e91bdf99a69f8dd6077db2771bf29a0ec79789db
Robin Tram <robin.tram@gmail.com>
2018-10-21 16:22:30 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -89,7 +89,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.where()
 end
 
 def can_destroy_a_single_item
